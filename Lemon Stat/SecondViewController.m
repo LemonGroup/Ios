0cce807ec6707934e8d722789dfb6526dae2363f
Arkadiy Grigoryanc <dragon_500@mail.ru>
2016-11-13 19:10:33 +0300

    made controller layout and add a table and mandatory methods «UITableViewDataSource

--- Lemon Stat/SecondViewController.m
+++ Lemon Stat/SecondViewController.m
@@ -8,7 +8,15 @@
 
 #import "SecondViewController.h"
 
-@interface SecondViewController ()
+#import <AFNetworking/AFNetworking.h>
+
+static NSString *person = @"Путин";
+
+@interface SecondViewController () {
+    NSArray *responseJSON;
+}
+
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -17,6 +25,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self loadData];
 }
 
 
@@ -25,5 +35,52 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - AFNetworking
+
+- (void)loadData {
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    
+    [manager GET:@"http://lemonstat.usite.pro/StatisticDetailFake.json"
+      parameters:nil
+        progress:^(NSProgress * _Nonnull downloadProgress) {
+            
+        }
+         success:^(NSURLSessionTask * _Nonnull task, id  _Nullable responseObject) {
+             responseJSON = [responseObject valueForKey:@"response"];
+             [self.tableView reloadData];
+             NSLog(@"JSON: %@", responseObject);
+         }
+         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+             NSLog(@"Error: %@", error);
+         }];
+    
+}
+
+#pragma mark - UITableViewDelegate
+
+
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return responseJSON.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *identifier = @"DateCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier forIndexPath:indexPath];
+    
+    cell.textLabel.text = [responseJSON[indexPath.row] valueForKey:@"person"];
+    cell.detailTextLabel.text = [responseJSON[indexPath.row] valueForKey:@"number"];
+    
+    return cell;
+}
 
 @end
