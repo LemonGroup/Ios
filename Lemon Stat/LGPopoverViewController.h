c459badc5fc0905129e322f0b0b9e4b0d7391bef
Arkadiy Grigoryanc <dragon_500@mail.ru>
2016-11-17 03:59:27 +0300

    create LGPopoverViewController

---
+++ Lemon Stat/LGPopoverViewController.h
@@ -0,0 +1,13 @@
+//
+//  LGPopoverViewController.h
+//  Lemon Stat
+//
+//  Created by Arkadiy Grigoryanc on 17.11.16.
+//  Copyright © 2016 Decidion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LGPopoverViewController : UIViewController
+
+@end
