96eacdd0a88e376c2cd11be7783504472773cea7
Arkadiy Grigoryanc <dragon_500@mail.ru>
2016-11-23 19:48:37 +0300

    работа над авторизацией пользователя

--- Lemon Stat/LGAuthViewController.m
+++ Lemon Stat/LGAuthViewController.m
@@ -9,15 +9,20 @@
 #import "LGAuthViewController.h"
 #import <AFNetworking/AFNetworking.h>
 
-@interface LGAuthViewController ()
-@property (weak, nonatomic) IBOutlet UIView *yellowLayer;
+#import "LGTabBarController.h"
 
-@property (weak, nonatomic) IBOutlet UIButton *enterButton;
+#import "NSString+Request.h"
+
+@interface LGAuthViewController () <UITextFieldDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextField *loginTextField;
-
 @property (weak, nonatomic) IBOutlet UITextField *passwordTextField;
+@property (weak, nonatomic) IBOutlet UILabel *responseLabel;
 
-@property (strong, nonatomic) NSString * token;
+@property (strong, nonatomic) NSString *token;
+@property (assign, nonatomic) NSInteger groupID;
+@property (assign, nonatomic) NSInteger privilege;
+
 @end
 
 @implementation LGAuthViewController
@@ -25,8 +30,6 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.enterButton.layer.cornerRadius = 5;
-    self.yellowLayer.layer.cornerRadius = 5;
 
 }
 
@@ -34,51 +37,102 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)enterButton:(id)sender {
-    
-    
-    NSString * urlString = [NSString stringWithFormat:@"http://yrsoft.cu.cc:8080/user/auth?user=%@&pass=%@",self.loginTextField.text,self.passwordTextField.text];
+
+#pragma mark - Requests
+
+- (void)requestAuth {
     
     AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
     
-    [manager GET:urlString
+    NSString *requestString = [self requestString];
+    
+    [manager GET:requestString
       parameters:nil
-        progress:^(NSProgress * _Nonnull downloadProgress) {
-            
-        }
+        progress:nil
          success:^(NSURLSessionTask * _Nonnull task, id  _Nullable responseObject) {
-             self.token = responseObject[@"token"];
-             NSLog(@"-------------------TOKEN-JSON: %@", self.token);
+             NSLog(@"success");
+             if (responseObject) {
+                                  
+                 _token = [responseObject valueForKey:@"token"];
+                 _groupID = [[responseObject valueForKey:@"groupId"] integerValue];
+                 _privilege = [[responseObject valueForKey:@"privilege"] integerValue];
+                 
+                 [self presentNavigationController];
+                 NSLog(@"++");
+             } else {
+                 
+                 _responseLabel.text = @"Неверное сочитание логина и пароля";
+                 NSLog(@"--");
+             }
+             
+             NSLog(@"-------------------TOKEN-JSON: %@", responseObject);
+             
          }
          failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
              NSLog(@"Error: %@", error);
          }];
-    
-    [manager.requestSerializer setValue:[NSString stringWithFormat:@"%@", self.token] forHTTPHeaderField:@"Auth-Token"];
-    
-    //-------------------
-        /*
-    [manager HEAD :[NSString stringWithFormat:@"Auth-Token: %@",self.token] parameters:nil success: nil   failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-        NSLog(@"Error: %@", error);
-    }];
-    
-    
-    //-------------------
+}
 
+- (NSString *)requestString {
     
-    [manager GET:@"http://yrsoft.cu.cc:8080/catalog/catalogs"
-      parameters:nil
-        progress:^(NSProgress * _Nonnull downloadProgress) {
-            
-        }
-         success:^(NSURLSessionTask * _Nonnull task, id  _Nullable responseObject) {
-             NSLog(@"-------------------JSON WITH TOKEN: %@", responseObject);
-         }
-         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
-             NSLog(@"Error: %@", error);
-         }];
-     */
+    NSString *string = [NSString stringWithFormat:@"http://yrsoft.cu.cc:8080/user/auth?user=%@&pass=%@",self.loginTextField.text,self.passwordTextField.text];
     
+    return [string encodeURLString];
+}
+
+#pragma mark - Actions
+
+- (BOOL)verificationFillingOfFields {
+    
+    if ([_loginTextField.text length] == 0 && [_passwordTextField.text length] == 0) {
+        _responseLabel.text = @"Введите логин и пароль";
+        return NO;
+    } else if ([_loginTextField.text length] == 0) {
+        _responseLabel.text = @"Введите логин";
+        return NO;
+    } else if ([_passwordTextField.text length] == 0) {
+        _responseLabel.text = @"Введите пароль";
+        return NO;
+    }
+    
+    return YES;
+}
+
+- (IBAction)actionJoin:(id)sender {
+    
+    if ([self verificationFillingOfFields]) {
+        [self requestAuth];
+    }
+}
+
+- (void)presentNavigationController {
+        
+    UINavigationController *navigationController = [self.storyboard instantiateViewControllerWithIdentifier:@"NavigationController"];
+    
+    [self presentViewController:navigationController animated:YES completion:nil];
+    
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (void)textFieldDidBeginEditing:(UITextField *)textField {
+    _responseLabel.text = @"";
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    
+    if ([textField isEqual:_loginTextField]) {
+        
+        [_passwordTextField becomeFirstResponder];
+        
+    } else if ([textField isEqual:_passwordTextField]) {
+        
+        [_passwordTextField resignFirstResponder];
+        
+        [self actionJoin:nil];
+        
+    }
+    return YES;
 }
 
 /*
@@ -90,10 +144,14 @@
     // Pass the selected object to the new view controller.
 }
 */
-- (IBAction)loginpassEdittingChanged:(id)sender {
-    if([self.loginTextField.text length] == 0 | [self.passwordTextField.text length] ==0)
-    self.enterButton.userInteractionEnabled = NO;
-    else self.enterButton.userInteractionEnabled = YES;
-}
+
+//- (IBAction)loginpassEdittingChanged:(id)sender {
+//    
+//    if([self.loginTextField.text length] == 0 || [self.passwordTextField.text length] == 0) {
+//        self.enterButton.userInteractionEnabled = NO;
+//    } else {
+//        self.enterButton.userInteractionEnabled = YES;
+//    }
+//}
 
 @end
